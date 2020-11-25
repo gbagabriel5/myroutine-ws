3c9b6e0321344161280acd288994ea042d06b8ed
Gabriel <gbagabriel5@outlook.com>
2020-11-25 13:18:50 -0400

    Removing unusable method

--- src/main/java/com/example/caravan/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/caravan/service/impl/UserServiceImpl.java
@@ -78,8 +78,4 @@ public class UserServiceImpl implements UserService {
         Optional<User> user = repository.findByEmailAndPassword(email, password);
         return user.isPresent();
     }
-
-    public void deleteTask(Integer id) {
-        repository.deleteTaskById(id);
-    }
 }
